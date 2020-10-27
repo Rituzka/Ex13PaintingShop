5e07659f744bead45b88683c4926a4ec8774c74b
Rita <rituzka@gmail.com>
2020-10-27 11:04:03 +0100

    project initialized and gitignore modify

---
+++ src/main/java/com/itAcademy/ex13paintingshop/Ex13paintingshopApplication.java
@@ -0,0 +1,13 @@
+package com.itAcademy.ex13paintingshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex13paintingshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex13paintingshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itAcademy/ex13paintingshop/Ex13paintingshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itAcademy.ex13paintingshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex13paintingshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
