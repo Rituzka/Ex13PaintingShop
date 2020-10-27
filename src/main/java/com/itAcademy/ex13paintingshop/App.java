1e4971da6b5e085c24b5d48277f139bf0d00b36c
Rita <rituzka@gmail.com>
2020-10-27 11:14:31 +0100

    project structure, packages

--- src/main/java/com/itAcademy/ex13paintingshop/App.java
+++ src/main/java/com/itAcademy/ex13paintingshop/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ex13paintingshopApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ex13paintingshopApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
